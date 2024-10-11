688e70509458f9d1dc38893fd09c0854ba80fadd
Brandon Scott <scottinnovationgroup@gmail.com>
2024-10-11 15:17:49 -0400

    Update get_assessment_cost_and_time.php

--- get_assessment_cost_and_time.php
+++ get_assessment_cost_and_time.php
@@ -2,8 +2,8 @@
 
 $input = json_decode($_POST['data']);
 
-$output['min_price'] = $input[1][7];
-$output['max_price'] = $input[1][8];
+$output['min_price'] = $input[1][8];
+$output['max_price'] = $input[1][9];
 $output['start_date'] = $input[1][4];
 $output['end_date'] = $input[1][5];
 
