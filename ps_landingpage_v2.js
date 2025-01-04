a5f58e8a105a7107535fd97f0a6bc123e4dcbd96
Brandon Scott <scottinnovationgroup@gmail.com>
2025-01-03 21:23:38 -0500

    Update ps_landingpage_v2.js

--- ps_landingpage_v2.js
+++ ps_landingpage_v2.js
@@ -1,3 +1,212 @@
+function getInitiativeTitles(jsonData, industry) {
+
+    var initiatives = JSON.parse(industryResponse)[industry]["initiatives"];
+    var titles = [];
+
+    // Loop through each initiative and extract the title
+    $.each(initiatives, function(index, initiative) {
+        titles.push(initiative.title);
+    });
+
+    return titles;
+}
+
+function createFields(fieldID, data) {
+    $('#'+fieldID+' option').remove();
+    $('#'+fieldID).append('<option value=""></option>');
+    $.each(data, function(index, item) {
+        $('#'+fieldID).append('<option value="'+item+'">'+item+'</option>');
+    });
+}
+
+function updateSolutionSelectFieldV2(nestedObject, selectFieldId, other=false) {
+
+    // Get the select field by ID
+    const selectField = document.getElementById(selectFieldId);
+
+    // Clear existing options
+    selectField.innerHTML = "";
+
+    // Create and append a default empty option
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = "";
+    selectField.appendChild(defaultOption);
+
+    //Add new options from the nested object array
+    nestedObject.forEach(value => {
+        const option = document.createElement("option");
+        option.value = value;
+        option.textContent = value;
+        selectField.appendChild(option);
+    });
+
+    if(other) {
+        $(selectField).append('<option value="Other">Other</option>');
+    }
+
+}
+
+function ajaxIndustryResponse(response, request) {
+
+    industryResponse = response;
+    industryRequest = request;
+
+    if(industryResponse != '{"result":"false"}') {
+        var titles = getInitiativeTitles(industryResponse, industryRequest);
+        createFields('Solution-Area', titles);
+
+        $('select#Solution-Area').on('change',function(){
+            console.log('bp1');
+            var changedElement = $(this);
+            $.each(JSON.parse(industryResponse)[$('#Industry-2').val()]["initiatives"], function(index, initiative) {
+
+                if(changedElement.val() == '') {
+                    $('.solution-field-group-wrapper').css("display","none");
+                    $('.solution-field-group-wrapper').css("visibility","hidden");
+                    $('.solution-field-group-wrapper select').val("");
+                    $('div.form-wrapper-landing-page').animate({
+                        height: $('div[aria-label="4 of 5"]').height() + 30 + 'px'
+                    }, 500);
+                } else {
+                    $('.solution-field-group-wrapper').css("visibility","visible");
+                    $('.solution-field-group-wrapper').css("display","flex");
+                }
+
+                if(initiative.title == changedElement.val()) {
+                    $('.form-label-focus-area-span').text(initiative.title.toLowerCase());
+                    updateSolutionSelectFieldV2(initiative.business_objectives, 'Primary-Objective', true);
+                    updateSolutionSelectFieldV2(initiative.success_measures, 'Current-Measures', true);
+                    updateSolutionSelectFieldV2(initiative.implementation_challenges, 'Challenges', true);
+                    updateSolutionSelectFieldV2(initiative.areas_of_improvement, 'Areas-for-Improvement', true);
+
+                    $('div.form-wrapper-landing-page').animate({
+                        height: $('div[aria-label="5 of 5"]').height() + 30 + 'px'
+                    }, 500);
+                }
+            });
+        });
+
+        return true;
+    } else {
+        $('#Slide-5-Wrapper .form-about-section').html(window.fallbackObjectivesHTML);
+        $('select#Solution-Area').on('change', setFallbackObjectives);
+        console.log('bp4');
+        return industryResponse;
+    }
+}
+
+function nextSlideButtonAjax(event='') {
+    $.ajax({
+        url: event.data.endpoint,
+        method: 'GET',
+        success: function(response) {
+            console.log(ajaxIndustryResponse(response));
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error('Error:', textStatus, errorThrown);
+            // Handle any errors here
+        }
+    }); // end ajax
+}
+
+function updateSelectField(nestedObject, selectFieldId) {
+    // Get the select field by ID
+    const selectField = document.getElementById(selectFieldId);
+
+    if (!selectField || !nestedObject[selectFieldId]) {
+        console.error("Invalid select field ID or nested object name.");
+        return;
+    }
+
+    // Clear existing options
+    selectField.innerHTML = "";
+
+    // Create and append a default empty option
+    const defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = "";
+    selectField.appendChild(defaultOption);
+
+    // Add new options from the nested object array
+    nestedObject[selectFieldId].forEach(value => {
+        const option = document.createElement("option");
+        option.value = value;
+        option.textContent = value;
+        selectField.appendChild(option);
+    });
+
+}
+
+function setFallbackObjectives() {
+    $('.other-option-input-section').remove();
+
+    if($(this).val() == '') {
+        $('.solution-field-group-wrapper').css("visibility","hidden");
+        $('.solution-field-group-wrapper select').val("");
+
+        $('div.form-wrapper-landing-page').animate({
+            height: $('div[aria-label="4 of 5"]').height() + 30 + 'px'
+        }, 500);
+    } else {
+        $('.solution-field-group-wrapper').css("visibility","visible");
+        $('.solution-field-group-wrapper').css("display","flex");
+
+        if($(this).val() == 'Brand and Marketing Strategy') {
+            $('.solution-field-group-wrapper').css("visibility","visible");
+            $('.form-label-focus-area-span').text('brand and marketing strategy');
+            updateSelectField(brandAndMarketing, 'Primary-Objective');
+            updateSelectField(brandAndMarketing, 'Current-Measures');
+            updateSelectField(brandAndMarketing, 'Challenges');
+            updateSelectField(brandAndMarketing, 'Areas-for-Improvement');
+        }
+        if($(this).val() == 'Business Transformation') {
+            $('.solution-field-group-wrapper').css("visibility","visible");
+            $('.form-label-focus-area-span').text('business transformation');
+            updateSelectField(businessTransformation, 'Primary-Objective');
+            updateSelectField(businessTransformation, 'Current-Measures');
+            updateSelectField(businessTransformation, 'Challenges');
+            updateSelectField(businessTransformation, 'Areas-for-Improvement');
+        }
+        if($(this).val() == 'Organizational Change Management') {
+            $('.solution-field-group-wrapper').css("visibility","visible");
+            $('.form-label-focus-area-span').text('organizational change management');
+            updateSelectField(organizationalChangeManagement, 'Primary-Objective');
+            updateSelectField(organizationalChangeManagement, 'Current-Measures');
+            updateSelectField(organizationalChangeManagement, 'Challenges');
+            updateSelectField(organizationalChangeManagement, 'Areas-for-Improvement');
+        }
+        if($(this).val() == 'IT Service Delivery') {
+            $('.solution-field-group-wrapper').css("visibility","visible");
+            $('.form-label-focus-area-span').text('IT service delivery');
+            updateSelectField(itServiceDelivery, 'Primary-Objective');
+            updateSelectField(itServiceDelivery, 'Current-Measures');
+            updateSelectField(itServiceDelivery, 'Challenges');
+            updateSelectField(itServiceDelivery, 'Areas-for-Improvement');
+        }
+        if($(this).val() == 'Product Innovation and Development') {
+            $('.solution-field-group-wrapper').css("visibility","visible");
+            $('.form-label-focus-area-span').text('product innovation and development');
+            updateSelectField(productInnovationAndDevelopment, 'Primary-Objective');
+            updateSelectField(productInnovationAndDevelopment, 'Current-Measures');
+            updateSelectField(productInnovationAndDevelopment, 'Challenges');
+            updateSelectField(productInnovationAndDevelopment, 'Areas-for-Improvement');
+        }
+        if($(this).val() == 'Startup Growth and Expansion') {
+            $('.solution-field-group-wrapper').css("visibility","visible");
+            $('.form-label-focus-area-span').text('startup growth and expansion');
+            updateSelectField(startupGrowthAndExpansion, 'Primary-Objective');
+            updateSelectField(startupGrowthAndExpansion, 'Current-Measures');
+            updateSelectField(startupGrowthAndExpansion, 'Challenges');
+            updateSelectField(startupGrowthAndExpansion, 'Areas-for-Improvement');
+        }
+
+        $('div.form-wrapper-landing-page').animate({
+            height: $('div[aria-label="5 of 5"]').height() + 30 + 'px'
+        }, 500);
+    }
+}
+
 const brandAndMarketing = {
     "Primary-Objective": [
         "Enhance Brand Awareness",
@@ -327,104 +536,46 @@ const startupGrowthAndExpansion = {
     ]
 };
 
-function updateSelectField(nestedObject, selectFieldId) {
-    // Get the select field by ID
-    const selectField = document.getElementById(selectFieldId);
+$(document).ready(function() {
 
-    if (!selectField || !nestedObject[selectFieldId]) {
-        console.error("Invalid select field ID or nested object name.");
-        return;
-    }
+    var industryResponse;
+    var fallbackObjectivesHTML = $('#Slide-5-Wrapper .form-about-section').html();
 
-    // Clear existing options
-    selectField.innerHTML = "";
+    $('.solution-field-group-wrapper').css("visibility","hidden");
+    $('select#Solution-Area').on('change', setFallbackObjectives);
 
-    // Create and append a default empty option
-    const defaultOption = document.createElement("option");
-    defaultOption.value = "";
-    defaultOption.textContent = "";
-    selectField.appendChild(defaultOption);
-
-    // Add new options from the nested object array
-    nestedObject[selectFieldId].forEach(value => {
-        const option = document.createElement("option");
-        option.value = value;
-        option.textContent = value;
-        selectField.appendChild(option);
+    $('#Industry-2').on('change', function(){
+        $('.solution-field-group-wrapper').css("display","none");
+        $('.solution-field-group-wrapper').css("visibility","hidden");
+        $('#Slide-5-Wrapper select').val('');
     });
 
-}
+    $('#Next-Slide-2').on('click press', function() {
+        var industryRequest = $('#Industry-2').val();
 
-function setFallbackObjectives() {
-    $('.other-option-input-section').remove();
+        if (industryRequest !== '') {
+            var endpoint = 'https://ps-json-to-html-table-43c5e8c6dd96.herokuapp.com/get_industry.php?item='+industryRequest;
 
-    if($(this).val() == '') {
-        $('.solution-field-group-wrapper').css("visibility","hidden");
-        $('.solution-field-group-wrapper select').val("");
+            $.ajax({
+                url: endpoint,
+                method: 'GET',
+                success: function(response) {
+                    if(ajaxIndustryResponse(response, industryRequest) === true) {
+                        $('#Next-Slide-3,#Next-Slide-4,#Next-Slide-5').off('click press', nextSlideButtonAjax);
+                        console.log('bp2');
+                    } else {
+                        console.log('bp3');
 
-        $('div.form-wrapper-landing-page').animate({
-            height: $('div[aria-label="4 of 5"]').height() + 30 + 'px'
-        }, 500);
-    } else {
-        $('.solution-field-group-wrapper').css("visibility","visible");
-        $('.solution-field-group-wrapper').css("display","flex");
+                        var endpoint_check_only = 'https://ps-json-to-html-table-43c5e8c6dd96.herokuapp.com/get_industry.php?item='+industryRequest+'&check_only=true';
 
-        if($(this).val() == 'Brand and Marketing Strategy') {
-            $('.solution-field-group-wrapper').css("visibility","visible");
-            $('.form-label-focus-area-span').text('brand and marketing strategy');
-            updateSelectField(brandAndMarketing, 'Primary-Objective');
-            updateSelectField(brandAndMarketing, 'Current-Measures');
-            updateSelectField(brandAndMarketing, 'Challenges');
-            updateSelectField(brandAndMarketing, 'Areas-for-Improvement');
+                        $('#Next-Slide-3,#Next-Slide-4,#Next-Slide-5').on('click press', {endpoint: endpoint_check_only}, nextSlideButtonAjax);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.error('Error:', textStatus, errorThrown);
+                    // Handle any errors here
+                }
+            });
         }
-        if($(this).val() == 'Business Transformation') {
-            $('.solution-field-group-wrapper').css("visibility","visible");
-            $('.form-label-focus-area-span').text('business transformation');
-            updateSelectField(businessTransformation, 'Primary-Objective');
-            updateSelectField(businessTransformation, 'Current-Measures');
-            updateSelectField(businessTransformation, 'Challenges');
-            updateSelectField(businessTransformation, 'Areas-for-Improvement');
-        }
-        if($(this).val() == 'Organizational Change Management') {
-            $('.solution-field-group-wrapper').css("visibility","visible");
-            $('.form-label-focus-area-span').text('organizational change management');
-            updateSelectField(organizationalChangeManagement, 'Primary-Objective');
-            updateSelectField(organizationalChangeManagement, 'Current-Measures');
-            updateSelectField(organizationalChangeManagement, 'Challenges');
-            updateSelectField(organizationalChangeManagement, 'Areas-for-Improvement');
-        }
-        if($(this).val() == 'IT Service Delivery') {
-            $('.solution-field-group-wrapper').css("visibility","visible");
-            $('.form-label-focus-area-span').text('IT service delivery');
-            updateSelectField(itServiceDelivery, 'Primary-Objective');
-            updateSelectField(itServiceDelivery, 'Current-Measures');
-            updateSelectField(itServiceDelivery, 'Challenges');
-            updateSelectField(itServiceDelivery, 'Areas-for-Improvement');
-        }
-        if($(this).val() == 'Product Innovation and Development') {
-            $('.solution-field-group-wrapper').css("visibility","visible");
-            $('.form-label-focus-area-span').text('product innovation and development');
-            updateSelectField(productInnovationAndDevelopment, 'Primary-Objective');
-            updateSelectField(productInnovationAndDevelopment, 'Current-Measures');
-            updateSelectField(productInnovationAndDevelopment, 'Challenges');
-            updateSelectField(productInnovationAndDevelopment, 'Areas-for-Improvement');
-        }
-        if($(this).val() == 'Startup Growth and Expansion') {
-            $('.solution-field-group-wrapper').css("visibility","visible");
-            $('.form-label-focus-area-span').text('startup growth and expansion');
-            updateSelectField(startupGrowthAndExpansion, 'Primary-Objective');
-            updateSelectField(startupGrowthAndExpansion, 'Current-Measures');
-            updateSelectField(startupGrowthAndExpansion, 'Challenges');
-            updateSelectField(startupGrowthAndExpansion, 'Areas-for-Improvement');
-        }
-
-        $('div.form-wrapper-landing-page').animate({
-            height: $('div[aria-label="5 of 5"]').height() + 30 + 'px'
-        }, 500);
-    }
-}
-
-
-// If solution area field is selected, run updatedSelectField functions for each of the four fields
-$('.solution-field-group-wrapper').css("visibility","hidden");
-$('select#Solution-Area').on('change', setFallbackObjectives);
+    });
+});
